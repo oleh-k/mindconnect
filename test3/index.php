16a6ed7275cb949c4eae09d18412ec8a41cbd822
oleh_ok <ok-ey@hotmail.com>
2024-09-14 16:15:01 +0300

    able to test it

--- test3/index.php
+++ test3/index.php
@@ -1,8 +1,18 @@
 <?php
 
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "mindconnect";
+
+$connection = new mysqli($servername, $username, $password, $dbname);
+$ids = [1,2];
 
 $data = [];
 foreach ($ids as $id) {
-    $result = $connection->query("SELECT `x, `y` FROM `values` WHERE `id` = " . $id);
+    $result = $connection->query("SELECT `x`, `y` FROM `values` WHERE `id` = " . $id);
     $data[] = $result->fetch_row();
-}
+} 
+
+echo '<pre>';
+var_dump($data);
