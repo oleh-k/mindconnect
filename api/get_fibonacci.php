8393f9688af298f799129e5f70e1630221ee2900
oleh_ok <ok-ey@hotmail.com>
2024-09-15 22:15:23 +0300

    validation input data

---
+++ api/get_fibonacci.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('../autoloader.php');
+
+
+# validation
+
+$validation_error = true;
+
+if (isset($_REQUEST['name']) || isset($_REQUEST['user_input'])) {
+    $name = $_REQUEST['name'];
+    $user_input = $_REQUEST['user_input'];
+
+    if (Validator::int($user_input) && Validator::text($name)) {
+        $validation_error = false;
+    }
+}
+
+if ($validation_error == true) {
+    $data['success'] = false;
+    $data['data'] = 'validation_error';
+    echo json_encode($data);
+    exit();
+}
+
