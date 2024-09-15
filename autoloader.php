58fab4be280d25bf84ba0472ed0a8c55c0cd9c48
oleh_ok <ok-ey@hotmail.com>
2024-09-15 21:29:50 +0300

    improve autoloader (add "class" folder to scan)

--- autoloader.php
+++ autoloader.php
@@ -4,9 +4,15 @@ spl_autoload_register(function ($class) {
     $baseDir = __DIR__ . '/';
 
     $file = $baseDir . str_replace('\\', '/', $class) . '.php';
-
     if (file_exists($file)) {
         require $file;
+    } else {
+
+        $file = $baseDir . 'class/' . str_replace('\\', '/', $class) . '.php';
+        
+        if (file_exists($file)) {
+            require $file;
+        }
     }
 });
 
