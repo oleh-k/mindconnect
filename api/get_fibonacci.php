baa2990a02e51042f78b2578e94b789c239542e1
oleh_ok <ok-ey@hotmail.com>
2024-09-15 22:46:55 +0300

    get Fibonacci and write into DB

--- api/get_fibonacci.php
+++ api/get_fibonacci.php
@@ -10,7 +10,7 @@ $validation_error = true;
 if (isset($_REQUEST['name']) && isset($_REQUEST['user_input'])) {
     $name = $_REQUEST['name'];
     $user_input = $_REQUEST['user_input'];
-
+    
     if (Validator::int($user_input) && Validator::text($name)) {
         $validation_error = false;
     }
@@ -23,3 +23,17 @@ if ($validation_error == true) {
     exit();
 }
 
+
+$fibonacci = Fibonacci::get($user_input);
+$ip = (isset($_SERVER['HTTP_CLIENT_IP'])?$_SERVER['HTTP_CLIENT_IP']:'');
+
+$db = new DB();
+$q = "INSERT INTO fibonacci (name, user_input, fibonacci_number, user_ip)
+        VALUES ('$name', $user_input, $fibonacci, '$ip');";
+$id = $db->query($q);
+
+
+$data['success'] = true;
+$data['data'] = $id;
+echo json_encode($data);
+exit();
